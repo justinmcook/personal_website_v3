e7bfa22a8b58ed1d4d5f1d00590759ed362f1e4d
Apomah <hockey.goalie1@live.ca>
2019-01-09 23:22:15 -0500

    Initial port to git

---
+++ js/script.js
@@ -0,0 +1,18 @@
+/**
+ * Created by Justin Cook on 7/19/17.
+ * The ~Index #~ shows what step of program, 0 is for anywhere
+ * Index format:
+ * ~Index Step.Substep.Substep.etc~
+ * Alternative to . is a - to indicate an option in a substep
+ * Examples:
+ * ~Index 15.15.15~ is Substep 15 of Substep 15 of Step 15
+ * ~Index 15.15-2~ is Option 2 of Substep 15 of Step 15
+ */
+$(document).ready(function(){
+	$(document).foundation();
+	/* Building Blocks */
+
+	/* Underline Menu Items */
+	$("[data-menu-underline-from-center] a").addClass("underline-from-center");
+	/************************/
+});
